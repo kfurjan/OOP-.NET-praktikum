4103e067b3625934e8c4ae8d01c817ea916fe3c6
Kevin Furjan <kfurjan@gmail.com>
2020-04-22 21:11:14 +0200

    Initial implementation of FlurlHttpClient

---
+++ DataAccessLayer/API/FlurlHttpClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Flurl;
+using Flurl.Http;
+
+namespace DataAccessLayer.API
+{
+    public static class FlurlHttpClient
+    {
+        public static async Task<IList<dynamic>> GetApiDataAsync()
+        {   
+            return await "https://world-cup-json-2018.herokuapp.com/matches".GetJsonAsync<IList<dynamic>>();
+        }
+    }
+}
---
+++ DataAccessLayer/Repository/RepositoryFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repository
+{
+    public static class RepositoryFactory
+    {
+        public static IRepository GetRepository()
+        {
+            return null;
+        }
+    }
+}

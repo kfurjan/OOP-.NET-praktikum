4618c5548281f2ae341cfbb5c5de9b766b9882c6
Kevin Furjan <kfurjan@gmail.com>
2020-04-23 18:26:32 +0200

    Added new functionality.

--- DataAccessLayer/API/FlurlHttpClient.cs
+++ DataAccessLayer/API/FlurlHttpClient.cs
@@ -1,20 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using Flurl;
+﻿using DataAccessLayer.Model;
 using Flurl.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DataAccessLayer.API
 {
     public static class FlurlHttpClient
     {
-        public static async Task<IList<dynamic>> GetApiDataAsync()
-        {   
-            return await "https://world-cup-json-2018.herokuapp.com/matches".GetJsonAsync<IList<dynamic>>();
+        private const string API_URL = "https://worldcup.sfg.io/";
+        private const string MATCHES_ENDPOINT = "matches/";
+        private const string TEAMS_ENDPOINT = "teams/";
+
+        private static async Task<IList<dynamic>> GetApiDataAsync(string endpoint) => await endpoint.GetJsonAsync<IList<dynamic>>();
+
+        public static async Task<IList<Team>> GetListAllTeamsAsync()
+        {
+            Task<IList<dynamic>> apiData = GetApiDataAsync($"{API_URL}{TEAMS_ENDPOINT}");
+            IList<dynamic> teamsData = await apiData;
+
+            IList<Team> teams = new List<Team>();
+            teamsData.ToList().ForEach(team =>
+            {
+                try
+                {
+                    teams.Add(new Team(
+                                (int)team.id,
+                                (string)team.country,
+                                (string)team.alternate_name,
+                                (string)team.fifa_code,
+                                (int)team.group_id,
+                                (string)team.group_letter));
+                }
+                catch (FlurlHttpException) { }
+            });
+
+            return teams;
         }
     }
 }
---
+++ DataAccessLayer/Model/Team.cs
@@ -0,0 +1,27 @@
+﻿namespace DataAccessLayer.Model
+{
+    public class Team
+    {
+        public Team() { }
+
+        public Team(int id, string country, string alternateName, string fifaCode, int groupId, string groupLetter)
+        {
+            Id = id;
+            Country = country;
+            AlternateName = alternateName;
+            FifaCode = fifaCode;
+            GroupId = groupId;
+            GroupLetter = groupLetter;
+        }
+
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public string AlternateName { get; set; }
+        public string FifaCode { get; set; }
+        public int GroupId { get; set; }
+        public string GroupLetter { get; set; }
+
+        public override string ToString()
+            => $"{Id} {Country} {AlternateName} {FifaCode} {GroupId} {GroupLetter}";
+    }
+}

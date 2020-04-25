e6931701b3ed44199ad882e4e3e0194955d3d104
Kevin Furjan <kfurjan@gmail.com>
2020-04-25 15:57:13 +0200

    Better abstraction

--- DataAccessLayer/API/FlurlHttpClient.cs
+++ DataAccessLayer/API/FlurlHttpClient.cs
@@ -1,9 +1,7 @@
-﻿using DataAccessLayer.Model;
+﻿using System.Collections.Generic;
 using Flurl.Http;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace DataAccessLayer.API
 {
@@ -14,62 +12,70 @@ namespace DataAccessLayer.API
         private const string MATCHES_ENDPOINT = @"matches/";
         private const string TEAMS_ENDPOINT = @"teams/";
 
-        private static async Task<IList<dynamic>> GetApiDataAsync(string endpoint) => await endpoint.GetJsonAsync<IList<dynamic>>();
+        public static async Task<string> GetApiDataAsync(string endpoint) => await endpoint.GetStringAsync();
 
-        public static async Task<IList<Team>> GetWomenTeamsAsync()
+        public static async Task<IEnumerable<T>> GetSomeClass<T>()
         {
-            IList<Team> teams = new List<Team>();
-            try
-            {
-                Task<IList<dynamic>> apiData = GetApiDataAsync($"{WOMEN_API_URL}{TEAMS_ENDPOINT}");
-                IList<dynamic> teamsData = await apiData;
+            var apiData = GetApiDataAsync($"{MEN_API_URL}{MATCHES_ENDPOINT}");
+            var data = await apiData;
 
-                teamsData.ToList().ForEach(team =>
-                    {
-                        try
-                        {
-                            teams.Add(new Team(
-                                        (int)team.id,
-                                        (string)team.country,
-                                        (string)team.alternate_name,
-                                        (string)team.fifa_code,
-                                        (int)team.group_id,
-                                        (string)team.group_letter));
-                        }
-                        catch (Exception) { }
-                    });
-            }
-            catch (FlurlHttpException) { throw; }
-
-            return teams;
+            return JsonConvert.DeserializeObject<IEnumerable<T>>(data);
         }
 
-        public static async Task<IList<Team>> GetMenTeamsAsync()
-        {
-            IList<Team> teams = new List<Team>();
-            try
-            {
-                Task<IList<dynamic>> apiData = GetApiDataAsync($"{MEN_API_URL}{TEAMS_ENDPOINT}");
-                IList<dynamic> teamsData = await apiData;
+        //public static async Task<IList<Team>> GetWomenTeamsAsync()
+        //{
+        //    IList<Team> teams = new List<Team>();
+        //    try
+        //    {
+        //        Task<IList<dynamic>> apiData = GetApiDataAsync($"{WOMEN_API_URL}{TEAMS_ENDPOINT}");
+        //        IList<dynamic> teamsData = await apiData;
 
-                teamsData.ToList().ForEach(team =>
-                {
-                    try
-                    {
-                        teams.Add(new Team(
-                                    (int)team.id,
-                                    (string)team.country,
-                                    (string)team.alternate_name,
-                                    (string)team.fifa_code,
-                                    (int)team.group_id,
-                                    (string)team.group_letter));
-                    }
-                    catch (Exception) { }
-                });
-            }
-            catch (FlurlHttpException) { throw; }
+        //        teamsData.ToList().ForEach(team =>
+        //            {
+        //                try
+        //                {
+        //                    teams.Add(new Team(
+        //                                (int)team.id,
+        //                                (string)team.country,
+        //                                (string)team.alternate_name,
+        //                                (string)team.fifa_code,
+        //                                (int)team.group_id,
+        //                                (string)team.group_letter));
+        //                }
+        //                catch (Exception) { }
+        //            });
+        //    }
+        //    catch (FlurlHttpException) { throw; }
 
-            return teams;
-        }
+        //    return teams;
+        //}
+
+        //public static async Task<IList<Team>> GetMenTeamsAsync()
+        //{
+        //    IList<Team> teams = new List<Team>();
+        //    try
+        //    {
+        //        Task<IList<dynamic>> apiData = GetApiDataAsync($"{MEN_API_URL}{TEAMS_ENDPOINT}");
+        //        IList<dynamic> teamsData = await apiData;
+
+        //        teamsData.ToList().ForEach(team =>
+        //        {
+        //            try
+        //            {
+        //                teams.Add(new Team(
+        //                            (int)team.id,
+        //                            (string)team.country,
+        //                            (string)team.alternate_name,
+        //                            (string)team.fifa_code,
+        //                            (int)team.group_id,
+        //                            (string)team.group_letter));
+        //            }
+        //            catch (Exception) { }
+        //        });
+        //    }
+        //    catch (FlurlHttpException) { throw; }
+
+        //    return teams;
+        //}
     }
 }
--- DataAccessLayer/Model/Team.cs
+++ DataAccessLayer/Model/Team.cs
@@ -1,28 +1,32 @@
-﻿namespace DataAccessLayer.Model
+﻿#nullable enable
+
+namespace DataAccessLayer.Model
 {
     public class Team
     {
+        private readonly int _id;
         public Team() { }
         public Team(int id, string country, string alternateName, string fifaCode, int groupId, string groupLetter)
         {
+            _id = id;
             Id = id;
             Country = country;
-            AlternateName = alternateName;
-            FifaCode = fifaCode;
-            GroupId = groupId;
-            GroupLetter = groupLetter;
+            Alternate_name = alternateName;
+            Fifa_code = fifaCode;
+            Group_id = groupId;
+            Group_letter = groupLetter;
         }
 
         public int Id { get; set; }
         public string Country { get; set; }
-        public string AlternateName { get; set; }
-        public string FifaCode { get; set; }
-        public int GroupId { get; set; }
-        public string GroupLetter { get; set; }
+        public string? Alternate_name { get; set; }
+        public string Fifa_code { get; set; }
+        public int Group_id { get; set; }
+        public string Group_letter { get; set; }
 
-        public override bool Equals(object obj) => obj is Team team ? Id == team.Id : false;
-        public override int GetHashCode() => Id;
+        public override bool Equals(object obj) => obj is Team team && Id == team.Id;
+        public override int GetHashCode() => _id;
         public override string ToString()
-            => $"{Id} {Country} {AlternateName} {FifaCode} {GroupId} {GroupLetter}";
+            => $"{Id} {Country} {Alternate_name} {Fifa_code} {Group_id} {Group_letter}";
     }
 }

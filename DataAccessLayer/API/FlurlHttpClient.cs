bc90c1722b78192938e6d86dfb4ce8988bf912af
Kevin Furjan <kfurjan@gmail.com>
2020-04-27 22:04:08 +0200

    LINQ ready data!!!

--- DataAccessLayer/API/FlurlHttpClient.cs
+++ DataAccessLayer/API/FlurlHttpClient.cs
@@ -7,75 +7,21 @@ namespace DataAccessLayer.API
 {
     public static class FlurlHttpClient
     {
-        private const string WOMEN_API_URL = @"https://worldcup.sfg.io/";
-        private const string MEN_API_URL = @"https://world-cup-json-2018.herokuapp.com/";
-        private const string MATCHES_ENDPOINT = @"matches/";
-        private const string TEAMS_ENDPOINT = @"teams/";
-
-        public static async Task<string> GetApiDataAsync(string endpoint) => await endpoint.GetStringAsync();
-
-        public static async Task<IEnumerable<T>> GetSomeClass<T>()
+        private static async Task<IEnumerable<dynamic>> GetApiDataAsync(string endpoint) => await endpoint.GetJsonAsync<IEnumerable<dynamic>>();
+        public static async Task<IEnumerable<dynamic>> GetJsonDataAsync(string endpoint)
         {
-            var apiData = GetApiDataAsync($"{MEN_API_URL}{MATCHES_ENDPOINT}");
-            var data = await apiData;
+            try
+            {
+                var apiData = GetApiDataAsync(endpoint);
+                var jObjects = await apiData;
 
-            return JsonConvert.DeserializeObject<IEnumerable<T>>(data);
+                return jObjects;
+            }
+            
+            catch (FlurlHttpException)
+            {
+                return new List<dynamic>();
+            }
         }
-
-        //public static async Task<IList<Team>> GetWomenTeamsAsync()
-        //{
-        //    IList<Team> teams = new List<Team>();
-        //    try
-        //    {
-        //        Task<IList<dynamic>> apiData = GetApiDataAsync($"{WOMEN_API_URL}{TEAMS_ENDPOINT}");
-        //        IList<dynamic> teamsData = await apiData;
-
-        //        teamsData.ToList().ForEach(team =>
-        //            {
-        //                try
-        //                {
-        //                    teams.Add(new Team(
-        //                                (int)team.id,
-        //                                (string)team.country,
-        //                                (string)team.alternate_name,
-        //                                (string)team.fifa_code,
-        //                                (int)team.group_id,
-        //                                (string)team.group_letter));
-        //                }
-        //                catch (Exception) { }
-        //            });
-        //    }
-        //    catch (FlurlHttpException) { throw; }
-
-        //    return teams;
-        //}
-
-        //public static async Task<IList<Team>> GetMenTeamsAsync()
-        //{
-        //    IList<Team> teams = new List<Team>();
-        //    try
-        //    {
-        //        Task<IList<dynamic>> apiData = GetApiDataAsync($"{MEN_API_URL}{TEAMS_ENDPOINT}");
-        //        IList<dynamic> teamsData = await apiData;
-
-        //        teamsData.ToList().ForEach(team =>
-        //        {
-        //            try
-        //            {
-        //                teams.Add(new Team(
-        //                            (int)team.id,
-        //                            (string)team.country,
-        //                            (string)team.alternate_name,
-        //                            (string)team.fifa_code,
-        //                            (int)team.group_id,
-        //                            (string)team.group_letter));
-        //            }
-        //            catch (Exception) { }
-        //        });
-        //    }
-        //    catch (FlurlHttpException) { throw; }
-
-        //    return teams;
-        //}
     }
 }
